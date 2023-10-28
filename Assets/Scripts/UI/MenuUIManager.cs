6ce384b5a1d94b286659eb474768377a7ed991e6
Jacob <JacobCohenIsrael@gmail.com>
2023-10-28 10:28:03 +0300

    created menu and game scene installed TMP

---
+++ Assets/Scripts/UI/MenuUIManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuUIManager : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    
+    private void Awake()
+    {
+        playButton.onClick.AddListener(OnPlayButtonClicked);
+    }
+
+    private void OnDestroy()
+    {
+        playButton.onClick.AddListener(OnPlayButtonClicked);
+    }
+
+    private void OnPlayButtonClicked()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
